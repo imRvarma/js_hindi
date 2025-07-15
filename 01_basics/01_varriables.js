063b3cb036c9f69f87f3274915be6aa9bf27b017
imRvarma <rahulichari@gmail.com>
2025-07-15 18:50:57 +0000

    varriables

---
+++ 01_basics/01_varriables.js
@@ -0,0 +1,15 @@
+const accountId = 14453
+let accountEmail = "rahulichari@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bihar"
+let accountState
+ /* 
+ Prefer not use var
+ beacuse it is old model use
+ */
+//accountId = 2
+accountPassword = "802203"
+accountEmail = "hitesh@gmail.com"
+accountCity ="patna"
+console.log(accountId); 
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
