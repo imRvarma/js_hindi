921ffa5476b8ecb28011f0202f5864244c3aca74
imRvarma <rahulichari@gmail.com>
2025-07-15 18:51:51 +0000

    data_types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //traet all code as newer version
+
+/*alert("hello")
+alert(3+3)//
+we are using node so we cant write like this you can use it in bindig html and console */
+
+console.log(3+3)// code readibility should be top
+
+console.log("Rahul")
+
+//DATA_TYPES
+
+let name ="rahul_kumar"//string dataTypes
+let age = 18//number data_Types
+let isloggedIn = false //boolean dataTypes
+
+//NUMBER =>2 TO POWER 53
+//bigInt
+//string =>""
+//boolean => true/false
+//null => standlonevalue
+//undefined =>
+    //symmbol -> for umiqueness
+
+//object dataTypes
+
+console.log(typeof 19)
+console.log(typeof age)
+console.log(typeof null)// object in output
+console.log(typeof undefined)//undefined in output
