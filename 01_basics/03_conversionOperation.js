e35c1db1e0c9cfd01e5406c1d1e581260ac4e7bb
imRvarma <rahulichari@gmail.com>
2025-07-16 17:47:10 +0000

    operational_operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,22 +1,48 @@
 let score = "33abc"
 
-console.log(typeof score);//or
-console.log(typeof (score));
+//console.log(typeof score);//or
+//console.log(typeof (score));
 
 let valueInnumber = Number(score)
-console.log(valueInnumber);
+//console.log(valueInnumber);
 
 let isLoggedin = 1
 let booleanIsloggedin =Boolean(isLoggedin)
-console.log(booleanIsloggedin);
+//console.log(booleanIsloggedin);
 
 let someNumber =33
 let stringNumber =String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
 //"33"=>33
 //"33abc"=>NaN
 //true => 1; false =>0
 
            //operations
+
+  let value =3
+  let negValue = -value
+//   console.log(negValue);  
+//   console.log(2+2);
+//   console.log(2-2);
+//   console.log(2*2);
+//   console.log(2**3);
+//   console.log(2%2);
+ 
+ let str1="hi"
+ let str2=" Rahul"
+ let str3= str1+str2
+// console.log(str3);
+
+// console.log(2+2+"4");//aise likhna sahi nhi hai
+
+// console.log((3+2)*3%4);//important hai aise code likhna nhi ki messey tarika se 
+
+           //tricky behavior is good but it is not rediabilty suitable so igone writting like this
+let num1 ,num2 ,num3
+num1=num2=num3=2+2
+
+let gameCounter =100
+++gameCounter;//prefix and suffix yaha humm padhenge ki kab lagane se kyaa hota hai aur kyaa nhi
+console.log(gameCounter);
