40998572619d1275ca6c4867cd7fc11f25d26d81
imRvarma <rahulichari@gmail.com>
2025-07-16 05:28:57 +0000

    conversionOfdatatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33abc"
+
+console.log(typeof score);//or
+console.log(typeof (score));
+
+let valueInnumber = Number(score)
+console.log(valueInnumber);
+
+let isLoggedin = 1
+let booleanIsloggedin =Boolean(isLoggedin)
+console.log(booleanIsloggedin);
+
+let someNumber =33
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true => 1; false =>0
+
+           //operations
