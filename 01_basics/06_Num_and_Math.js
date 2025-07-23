0ad68224105203e4228857f4f9f061cc94afbd27
imRvarma <rahulichari@gmail.com>
2025-07-23 07:43:22 +0000

    math_num_mathRandom

--- 01_basics/06_Num_and_Math.js
+++ 01_basics/06_Num_and_Math.js
@@ -1,28 +1,28 @@
 //+++++++++++++++++++++++++ NUMBER ++++++++++++++++++++
 
 const score=400
-//console.log(score);
+console.log(score);
 const balance=new Number(100)
-//console.log(balance);
+console.log(balance);
 
 //kuchh number ko define karne ka trika aur uske related jo baat hai usko discus yaha hum karenge
-//console.log(score.toExponential());
-//console.log(score.toFixed());
-//console.log(score.toLocaleString());
-//console.log(score.toPrecision());
-//console.log(score.valueOf());
-//console.log(score.toString().length);
-//console.log(score.toFixed(2));//value yaha fixed ho jayega decimal ke baad ka 2 point ko add ho jayega
+console.log(score.toExponential());
+console.log(score.toFixed());
+console.log(score.toLocaleString());
+console.log(score.toPrecision());
+console.log(score.valueOf());
+console.log(score.toString().length);
+console.log(score.toFixed(2));//value yaha fixed ho jayega decimal ke baad ka 2 point ko add ho jayega
 
 //precision ko check karne ka trika kaise check karenge aur kaise fixed karenge ex:value=23.342
 const value=23.342
-//console.log(value.toPrecision(3));//output=23.3,if precision value is 2 it will return in output=23 ,and if decimal will be grater than 6> then it will return 24 ex:value2=23.78
+console.log(value.toPrecision(3));//output=23.3,if precision value is 2 it will return in output=23 ,and if decimal will be grater than 6> then it will return 24 ex:value2=23.78
 const value2=23.78
-//console.log(value2.toPrecision(2)); //yaha 2nd position par value ko precise kiya jayega 
+console.log(value2.toPrecision(2)); //yaha 2nd position par value ko precise kiya jayega 
 
 const hundreds=1000000
-//console.log(hundreds.toLocaleString());//it will return in US value 1,000,000
-//console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString());//it will return in US value 1,000,000
+console.log(hundreds.toLocaleString('en-IN'));
 
 
 //+++++++++++++++++++ MATH ++++++++++++++++++++++++
@@ -39,7 +39,7 @@ console.log(Math.max(12,8,3,2,7,0));//output:12
 
 //++++++++++ random_math +++++++++++
 
-console.log(Math.random());
+console.log(Math.random());//it will always give return in 0-1.
 console.log((Math.random()*10)+1);
 console.log(Math.floor(Math.random()*10)+1);
 
